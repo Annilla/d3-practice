2a0a26f8420e0dcff62e0829d5483d62b22dbec6
Annilla <anny09117011@gmail.com>
2017-05-27 09:54:22 +0800

    base

--- src/js/router.js
+++ src/js/router.js
@@ -1,17 +1,22 @@
 import { Router } from 'director/build/director.js';
+let DEV_MODE = true;
+let routes;
 
-const routes = {
-  // 首頁
-  '/': require('./pages/index')['default'],
-  '/index.html': require('./pages/index')['default'],
-};
-
-// npm run prod use: /website2017/dist
-
-// const routes = {
-//   // 首頁
-//   '/d3-practice/dist/': require('./pages/index')['default'],
-//   '/d3-practice/dist/index.html': require('./pages/index')['default'],
-// };
+if (DEV_MODE) {
+  // npm run dev use: /
+  routes = {
+    // 首頁
+    '/': require('./pages/index')['default'],
+    '/index.html': require('./pages/index')['default'],
+  };
+}
+else {
+  // npm run prod use: /website2017/dist
+  routes = {
+    // 首頁
+    '/d3-practice/dist/': require('./pages/index')['default'],
+    '/d3-practice/dist/index.html': require('./pages/index')['default'],
+  };
+}
 
 export default new Router(routes);
